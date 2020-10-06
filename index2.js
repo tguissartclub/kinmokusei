669615b0ea66737bf6f0444e335963229c47c085
tguissartclub <71717434+tguissartclub@users.noreply.github.com>
2020-10-06 17:30:59 +0900

    Update index2.js

--- index2.js
+++ index2.js
@@ -20,7 +20,7 @@ var a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
     console.log(resultdata);
 }
 
-document.innerHTML += resultdata;
+console.log(resultdata);
 
 for (i = a.length - 1; i > 0; i--) {
   j = Math.floor(Math.random() * (i + 1));
